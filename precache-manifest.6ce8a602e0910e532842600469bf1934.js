fde389348ca608425fe69a58e1073e06c5d722af
0w1 <owltechdev@gmail.com>
2025-12-11 07:46:23 +0330

    Updates

---
+++ precache-manifest.6ce8a602e0910e532842600469bf1934.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d9fa878d157e18a9e4c5c2818cb3e84",
+    "url": "/react-jewelry-shop/index.html"
+  },
+  {
+    "revision": "f7879b7fe55ea99a1ddb",
+    "url": "/react-jewelry-shop/static/css/2.05757d0e.chunk.css"
+  },
+  {
+    "revision": "8bfaece92e1c07e8ddd7",
+    "url": "/react-jewelry-shop/static/css/main.83d727ec.chunk.css"
+  },
+  {
+    "revision": "f7879b7fe55ea99a1ddb",
+    "url": "/react-jewelry-shop/static/js/2.27c9ef7e.chunk.js"
+  },
+  {
+    "revision": "9a87d441d6f1d11956d517a23294f3df",
+    "url": "/react-jewelry-shop/static/js/2.27c9ef7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ef442479acf07b3d25",
+    "url": "/react-jewelry-shop/static/js/3.1f2e1642.chunk.js"
+  },
+  {
+    "revision": "8bfaece92e1c07e8ddd7",
+    "url": "/react-jewelry-shop/static/js/main.ef8a7323.chunk.js"
+  },
+  {
+    "revision": "3ee0063b7d5e18de05b5",
+    "url": "/react-jewelry-shop/static/js/runtime-main.304ebc80.js"
+  },
+  {
+    "revision": "ba49e844892321d8540ea3b7c088cf97",
+    "url": "/react-jewelry-shop/static/media/bootstrap-icons.ba49e844.woff"
+  },
+  {
+    "revision": "cc1e5eda776be5f0ff614285c31d4892",
+    "url": "/react-jewelry-shop/static/media/bootstrap-icons.cc1e5eda.woff2"
+  }
+]);
